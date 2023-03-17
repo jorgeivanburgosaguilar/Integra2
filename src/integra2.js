689fab89a6b78707ed7b8bdb788c291da795b43b
Jorge Ivan Burgos Aguilar <15068542+jorgeivanburgosaguilar@users.noreply.github.com>
2023-03-17 14:19:49 -0600

    quitar dom ready que añadi a la parte de footer y desactivar telemetria que tiene el componente.

--- src/integra2.js
+++ src/integra2.js
@@ -753,7 +753,7 @@ $(document).ready(function () {
   };
 
   function integra2_Uso(tipo) {
-    void(0);
+    void 0;
     // integra2_ip = 0;
 
     // var sel = $("script[src^='https://www.infomexsinaloa.org/accesibilidadweb/js_api.aspx']").attr(
@@ -1965,110 +1965,107 @@ function clic2() {
   }
 }
 
-// DOM Ready
-$(function () {
-  // EXTRAE LOS HIPERVINCULOS PARA LA VENTANA MODAL
-  var x = document.querySelectorAll("a");
+// EXTRAE LOS HIPERVINCULOS PARA LA VENTANA MODAL
+var x = document.querySelectorAll("a");
 
-  var integra2_myarray = [];
-  for (var i = 0; i < x.length; i++) {
-    var nametext = x[i].textContent;
-    var cleantext = nametext.replace(/\s+/g, " ").trim();
-    var cleanlink = x[i].href;
-    integra2_myarray.push([cleantext, cleanlink]);
-  }
-  function integra2_make_table() {
-    var integra2_tablelinks = "<table><thead><th></th></thead><tbody>";
-    for (var i = 0; i < integra2_myarray.length; i++) {
-      if (integra2_myarray[i][0].length > 0) {
-        integra2_tablelinks +=
-          "<tr><td><a href='" +
-          integra2_myarray[i][1] +
-          "' target='_blank'>" +
-          integra2_myarray[i][0] +
-          "</a></td></tr>";
-      }
+var integra2_myarray = [];
+for (var i = 0; i < x.length; i++) {
+  var nametext = x[i].textContent;
+  var cleantext = nametext.replace(/\s+/g, " ").trim();
+  var cleanlink = x[i].href;
+  integra2_myarray.push([cleantext, cleanlink]);
+}
+function integra2_make_table() {
+  var integra2_tablelinks = "<table><thead><th></th></thead><tbody>";
+  for (var i = 0; i < integra2_myarray.length; i++) {
+    if (integra2_myarray[i][0].length > 0) {
+      integra2_tablelinks +=
+        "<tr><td><a href='" +
+        integra2_myarray[i][1] +
+        "' target='_blank'>" +
+        integra2_myarray[i][0] +
+        "</a></td></tr>";
     }
-    document.getElementById("integra2_links").innerHTML = integra2_tablelinks;
-    jq_integra2_Uso("links");
   }
+  document.getElementById("integra2_links").innerHTML = integra2_tablelinks;
+  jq_integra2_Uso("links");
+}
 
-  // variable Modal y modal de links
-  var modal = document.getElementById("lemodale");
-  var modal2 = document.getElementById("lemodale2");
+// variable Modal y modal de links
+var modal = document.getElementById("lemodale");
+var modal2 = document.getElementById("lemodale2");
 
-  // variable del boton y se abre el modal y modal de links
-  var btn = document.getElementById("integra2_myBtn");
-  var btn2 = document.getElementById("integra2_Btnlinks");
+// variable del boton y se abre el modal y modal de links
+var btn = document.getElementById("integra2_myBtn");
+var btn2 = document.getElementById("integra2_Btnlinks");
 
-  // Variable del span que cierra el modal y modal de links
-  var span = document.getElementsByClassName("integra2_close")[0];
-  var span2 = document.getElementsByClassName("integra2_close2")[0];
+// Variable del span que cierra el modal y modal de links
+var span = document.getElementsByClassName("integra2_close")[0];
+var span2 = document.getElementsByClassName("integra2_close2")[0];
 
-  // Se abre el modal con un clic sobre el boton y modal de links
-  btn.onclick = function () {
-    modal.style.display = "block";
-  };
-  btn2.onclick = function () {
-    modal2.style.display = "block";
-  };
+// Se abre el modal con un clic sobre el boton y modal de links
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+btn2.onclick = function () {
+  modal2.style.display = "block";
+};
 
-  // Se cierra el modal con el <span> (x) y modal de links
-  span.onclick = function () {
-    $("#lemodale").hide();
-  };
-  span2.onclick = function () {
-    $("#lemodale2").hide();
-  };
+// Se cierra el modal con el <span> (x) y modal de links
+span.onclick = function () {
+  $("#lemodale").hide();
+};
+span2.onclick = function () {
+  $("#lemodale2").hide();
+};
 
-  // Se cierra el modal al hacer clic en cualquier parte de la pÃ¡gina
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  };
+// Se cierra el modal al hacer clic en cualquier parte de la pÃ¡gina
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
-  // El Modal se cierra con la tecla Esc y se abre con Ctrl+m
-  document.onkeydown = function (evt) {
-    evt = evt || window.event;
-    if (evt.keyCode == 27) {
+// El Modal se cierra con la tecla Esc y se abre con Ctrl+m
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 27) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
+  if (evt.ctrlKey && evt.keyCode == 77) {
+    if (modal.style.display === "block") {
       modal.style.display = "none";
       modal2.style.display = "none";
-    }
-    if (evt.ctrlKey && evt.keyCode == 77) {
-      if (modal.style.display === "block") {
-        modal.style.display = "none";
-        modal2.style.display = "none";
-      } else {
-        modal.style.display = "block";
-      }
-    }
-    if (evt.ctrlKey && evt.keyCode == 118) {
-      modal2.style.display = "block";
-      document.getElementById("integra2_text_focus").focus();
-      integra2_make_table();
-    }
-  };
-
-  // Funciones para la guia de lectura (la primera la pinta, la segunda la activa o desactiva)
-  var $readline = $("#readline");
-
-  $(document).on("mousemove", function (evt) {
-    $readline.css({ left: 0, top: evt.pageY + 10 });
-  });
-
-  function hiderule() {
-    var x = document.getElementById("readline");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
     } else {
-      x.style.display = "flex";
+      modal.style.display = "block";
     }
   }
-
-  function hiderule2() {
-    var x = document.getElementById("readline");
-    x.style.display = "none";
+  if (evt.ctrlKey && evt.keyCode == 118) {
+    modal2.style.display = "block";
+    document.getElementById("integra2_text_focus").focus();
+    integra2_make_table();
   }
-  // Funciones para la guia de lectura (la primera la pinta, la segunda la activa o desactiva)
+};
+
+// Funciones para la guia de lectura (la primera la pinta, la segunda la activa o desactiva)
+var $readline = $("#readline");
+
+$(document).on("mousemove", function (evt) {
+  $readline.css({ left: 0, top: evt.pageY + 10 });
 });
+
+function hiderule() {
+  var x = document.getElementById("readline");
+  if (x.style.display === "flex") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "flex";
+  }
+}
+
+function hiderule2() {
+  var x = document.getElementById("readline");
+  x.style.display = "none";
+}
+// Funciones para la guia de lectura (la primera la pinta, la segunda la activa o desactiva)
